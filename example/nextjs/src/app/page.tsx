e7c8905a71ee2e7228fb196d5938672596c1b326
jeheecheon <jeheecheon@gmail.com>
2026-04-04 23:09:47 +0900

    chore(example/nextjs): remove page title heading

--- example/nextjs/src/app/page.tsx
+++ example/nextjs/src/app/page.tsx
@@ -5,7 +5,6 @@ import SessionStorageDemo from "@/components/SessionStorageDemo";
 export default function Home() {
   return (
     <main className="max-w-lg mx-auto p-8 space-y-6">
-      <h1 className="text-zinc-400 text-sm font-mono">synced-storage / react example</h1>
       <CookieDemo />
       <LocalStorageDemo />
       <SessionStorageDemo />
