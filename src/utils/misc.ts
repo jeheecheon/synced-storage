47eabcd43ac9885d06c379dd04840f17f9222e77
jeheecheon <jeheecheon@gmail.com>
2026-04-06 18:00:59 +0900

    refactor: add console.warn to safelyGet, fix isFunction type, remove compareDeep

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -2,17 +2,13 @@ export function safelyGet<T>(callback: () => T): T | undefined {
   try {
     return callback();
   } catch (e) {
+    console.warn("[synced-storage]", e);
     return undefined;
   }
 }
 
-export function isFunction(value: any): value is (...args: any[]) => any {
+export function isFunction(value: unknown): value is (...args: any[]) => any {
   return typeof value === "function";
-}
-
-export function compareDeep(a: any, b: any): boolean {
-  // TODO: replace this with actual deep comparison
-  return JSON.stringify(a) === JSON.stringify(b);
 }
 
 export function isServer(): boolean {
